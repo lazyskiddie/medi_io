e3cb4e1c31bc4928aff797f4a133391b3021d7c4
lazyskiddie <sandeepsingh08550@gmail.com>
2026-04-02 09:46:48 +0530

    little bit 3

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
